0263cf3b80a3c30c4903f6dea89f195487f8eb94
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-11 13:33:46 +0530

    ``

--- src/lineIterator.js
+++ src/lineIterator.js
@@ -1,25 +1,30 @@
 const linesFound = [];
 
 function* lineIterator(text = '', delimiter = '') {
-  let i = 0;
+  let previousNewLine = 0;
 
+  while (previousNewLine!== -1 ) {
+    yield previousNewLine;
 
-  while (true) {
-    yield i;
-    const delIndex = text.indexOf(delimiter, i);
-    const line = text.slice(i, delIndex);
+    const delIndex = text.indexOf(delimiter, previousNewLine);
+    const line = text.slice(previousNewLine, delIndex);
 
-    i = delIndex + 1;
+    previousNewLine = delIndex + 1;
 
-    console.log({ i, delIndex });
+    console.log({ i: previousNewLine, delIndex });
     console.log({ line });
 
   }
 
 }
+
 const text = "hello\nthis\nis\nme\nfat\nyoda\nI\nhope\nyou\nare\nhaving\na\ngood\ntime\nbeing\nwith\nme";
 
 const lines = lineIterator(text, '\n');
+
+for (const element of lines) {
+  
+}
 // lines.next();
 // lines.next();
 // lines.next();
