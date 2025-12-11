eb295da1ed6c04b0ebf855227c116f13dcbbae9f
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-11 13:34:59 +0530

    ``

--- src/lineIterator.js
+++ src/lineIterator.js
@@ -3,17 +3,19 @@ const linesFound = [];
 function* lineIterator(text = '', delimiter = '') {
   let previousNewLine = 0;
 
-  while (previousNewLine!== -1 ) {
+
+  while (previousNewLine < text.length) {
     yield previousNewLine;
 
-    const delIndex = text.indexOf(delimiter, previousNewLine);
+    let delIndex = text.indexOf(delimiter, previousNewLine);
+    if (delIndex === -1) {
+      delIndex = text.length;
+    }
+
     const line = text.slice(previousNewLine, delIndex);
+    yield line;
 
     previousNewLine = delIndex + 1;
-
-    console.log({ i: previousNewLine, delIndex });
-    console.log({ line });
-
   }
 
 }
@@ -21,10 +23,14 @@ function* lineIterator(text = '', delimiter = '') {
 const text = "hello\nthis\nis\nme\nfat\nyoda\nI\nhope\nyou\nare\nhaving\na\ngood\ntime\nbeing\nwith\nme";
 
 const lines = lineIterator(text, '\n');
+const linesCopy = Iterator.from(lines);
 
-for (const element of lines) {
-  
-}
+const tokens = [...lines];
+
+// for (const x of lines) {
+//   console.log(x);
+// }
+
 // lines.next();
 // lines.next();
 // lines.next();
