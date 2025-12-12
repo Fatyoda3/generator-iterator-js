2559689bef2b57806cd43f804c71278113266492
Fatyoda3 <shivangsingh654@gmail.com>
2025-12-12 07:29:13 +0530

    `another one  `

---
+++ src/chunk_by.js
@@ -0,0 +1,15 @@
+function* chunkBy(size, values) {
+  const chunks = [];
+  for (let index = 0; index < values.length; index += size) {
+    chunks.push(values.slice(index, index + size));
+  }
+
+  yield chunks;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6,7];
+
+const chunkier = chunkBy(3, numbers);
+for (const el of chunkier) {
+  console.log(el);
+}
